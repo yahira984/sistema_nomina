f6d384de6b66459cda556fe7aeebc32f5853f04d
Kevin Yahir Avila <yahira984@gmail.com>
2026-06-08 09:10:08 -0600

    Correcion de excel editable, y salidas de los miercoles automaticas

--- app/Exports/ReciboIndividualExport.php
+++ app/Exports/ReciboIndividualExport.php
@@ -4,9 +4,13 @@ namespace App\Exports;
 
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Worksheet\PageSetup;
 
-class ReciboIndividualExport implements FromView, ShouldAutoSize
+class ReciboIndividualExport implements FromView, WithColumnWidths, WithEvents
 {
     protected $datos;
 
@@ -19,5 +23,59 @@ class ReciboIndividualExport implements FromView, ShouldAutoSize
     {
         // Pasamos los datos usando "with" para que Blade los reconozca como variables individuales
         return view('excel.recibo_individual', $this->datos);
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 28,
+            'B' => 18,
+            'C' => 28,
+            'D' => 18,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+
+                $sheet->setShowGridlines(false);
+                $sheet->setPrintGridlines(false);
+                $sheet->getPageSetup()
+                    ->setOrientation(PageSetup::ORIENTATION_PORTRAIT)
+                    ->setPaperSize(PageSetup::PAPERSIZE_LETTER)
+                    ->setFitToPage(true)
+                    ->setFitToWidth(1)
+                    ->setFitToHeight(1)
+                    ->setHorizontalCentered(true);
+
+                $sheet->getPageMargins()
+                    ->setTop(0.35)
+                    ->setRight(0.25)
+                    ->setBottom(0.35)
+                    ->setLeft(0.25);
+
+                $sheet->getPageSetup()->setPrintArea('A1:D9');
+
+                $sheet->getStyle('A1:D9')->getFont()->setName('Arial');
+                $sheet->getStyle('A1:D9')->getAlignment()
+                    ->setWrapText(true)
+                    ->setVertical(Alignment::VERTICAL_CENTER);
+                $sheet->getStyle('A6:D6')->getAlignment()->setVertical(Alignment::VERTICAL_TOP);
+
+                $sheet->getRowDimension(1)->setRowHeight(50);
+                $sheet->getRowDimension(2)->setRowHeight(25);
+                $sheet->getRowDimension(3)->setRowHeight(58);
+                $sheet->getRowDimension(4)->setRowHeight(24);
+                $sheet->getRowDimension(5)->setRowHeight(20);
+                $sheet->getRowDimension(6)->setRowHeight(112);
+                $sheet->getRowDimension(7)->setRowHeight(28);
+                $sheet->getRowDimension(8)->setRowHeight(28);
+                $sheet->getRowDimension(9)->setRowHeight(58);
+                $sheet->setSelectedCell('A1');
+            },
+        ];
     }
 }
